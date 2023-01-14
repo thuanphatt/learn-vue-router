0efd7d6d5f6afc6f2b32bef167eb95586ff27269
thuanphatt <zelorntp@gmail.com>
2023-01-14 22:47:09 +0700

    learn fetch using async & await

--- src/pages/TransactionPage.vue
+++ src/pages/TransactionPage.vue
@@ -11,19 +11,27 @@
       <p>Price: {{ transaction.price }}</p>
     </div>
   </div>
+  <div v-else-if="error">{{ error.message }}</div>
   <div v-else>Transaction Loading ...</div>
 </template>
 <script>
+import { ref } from "vue";
 export default {
-  data() {
-    return {
-      transactions: [],
+  setup() {
+    const transactions = ref([]);
+    const error = ref(null);
+    const fetchAll = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/transactions");
+        if (!response.ok) throw new Error("Something went wrong");
+        transactions.value = await response.json();
+      } catch (err) {
+        error.value = err;
+        console.log(error.value);
+      }
     };
-  },
-  created() {
-    fetch("http://localhost:3000/transactions")
-      .then((response) => response.json())
-      .then((data) => (this.transactions = data));
+    fetchAll();
+    return { transactions, error };
   },
 };
 </script>
